b1c220feccfce3229d96f72c6560ac486923e8a3
jackds10 <destinijackson2013@yahoo.com>
2020-10-23 17:48:17 -0400

    made changes

--- email.py
+++ email.py
@@ -24,3 +24,17 @@ while more == "y":
             more = input("Please enter a y or n: ")
 #prints email addresses    
 print(addresses)
+
+#print emails
+
+# url to add new user to the room
+add_membership_url = 'https://web.webex.com/spaces/aHR0cHM6Ly9jb252LWEud2J4Mi5jb20vY29udmVyc2F0aW9uL2FwaS92MS9jb252ZXJzYXRpb25zL2U2OTJkOWMwLTBlMjMtMTFlYi05MjZiLTg3N2ZlMzEyNjUxYw==
+'
+
+print
+for email in emails: # every email address in the lines list
+    try:
+        param = {
+        "roomId": SDN Postman Room,
+        "personEmail": destinijackson2013@yahoo.com,
+        }
